389b34df79301699ba5907afa5e2ef3b01f8800e
Victor Goecking <victorgoecking96@gmail.com>
2022-09-14 23:18:24 -0300

    Create routes and install prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+// const gameId = request.params.id;
+
   return response.json([
     {
       id: 1, name: 'John',
@@ -18,5 +28,11 @@ app.get('/ads', (request, response) => {
     },
   ])
 })
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+  
+    return response.json([])
+  })
 
 app.listen(3333)
